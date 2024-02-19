fd4125ad9e95b6d480dbfa1b0072468cf08c0d89
Tsubasa Watanabe <t.watanabe423@gmail.com>
2024-02-19 14:47:11 +0000

    upd

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { Button } from "./components/ui/button"
 import { Card } from "./components/ui/card"
 import { Input } from "./components/ui/input"
-import { escape } from "querystring"
+
 const buttonList = [
   "ダラダラ",
   "ワイワイ",
